76a3e098bf66ad3c46fb79fc9834908e508c124c
M-O-H <mohanishst@gmail.com>
2020-06-03 23:41:45 +0530

    Major done, only CSS left.

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,34 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, FlatList, Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Tile } from 'react-native-elements';
+import { PEOPLE } from "../shared/people";
+import { BIRTHDAYGUY } from "../shared/birthdayGuy"
 
+//TODO: MODAL ON START
 export default function Home({ navigation }) {
+    //Hooks
+    const [people, setPeople] = useState(PEOPLE);
+    const [showModal, toggleModal] = useState(true);
+    const [birthdayGuy, setBirthdayGuy] = useState(BIRTHDAYGUY)
+
+    const renderPeopleItem = ({ item, index }) => {
+        return (<Tile
+            key={index}
+            title={item.name}
+            caption={item.description}
+            featured
+            onPress={() => navigation.navigate("Person_i", { personId: item.id })}
+            //! Image see
+            imageSrc={require('./images/alberto.png')}
+        />);
+    };
+
     return (
         <View style={styles.container}>
             <Text>Home Screen of the app!</Text>
             <Button
-                title="Go to"
-                onPress={() => navigation.navigate('Person1')}
-            />
-            <Button
-                title="Person1 name"
-                onPress={() => navigation.navigate('Person2')}
-            />
-            <Button
-                title="Person2 name"
-                onPress={() => navigation.navigate('Person2')}
-            />
-            <Button
-                title="Person3 name"
-                onPress={() => navigation.navigate('Person3')}
-            />
-            <Button
                 title="Person_i name"
                 onPress={() => navigation.navigate('Person_i')}
+            />
+            <FlatList
+                data={people}
+                renderItem={renderPeopleItem}
+                keyExtractor={item => item.id.toString()}
             />
             <Button
                 title="Go to About App Section."
                 onPress={() => navigation.navigate('AboutApp')}
             />
+            <Modal animationType="slide" transparent={false}
+                visible={showModal}
+                onDismiss={() => toggleModal(!showModal)}
+                onRequestClose={() => toggleModal(!showModal)}>
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>Happy Birthday! {birthdayGuy[0].name}</Text>
+                    <Text style={styles.modalText}>Cheers on turning {birthdayGuy[0].age}!</Text>
+                    <Text>Come, Let's have  a blast!</Text>
+                    <Button
+                        onPress={() => toggleModal(!showModal)}
+                        color="#512DA8"
+                        title="SUrprizeee"
+                    />
+                </View>
+            </Modal>
         </View>
     );
 }
@@ -39,5 +64,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#555',
         alignItems: 'center',
         justifyContent: 'center',
+        paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
     },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#512DA8',
+        textAlign: 'center',
+        color: 'white',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
+    }
 });
--- components/Person_iComponent.js
+++ components/Person_iComponent.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Share } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import { PEOPLE } from "../shared/people";
+const limit = PEOPLE.length;
 
-export default function Person3() {
+
+export default function Person_i({ route, navigation }) {
+    // Params
+    const { personId } = route.params;
+    const nextPerson = () => {
+        if (personId < limit - 1) {
+            navigation.push('Person_i', { personId: personId + 1 })
+        }
+        else {
+            navigation.popToTop();
+        }
+    }
+
+    // FOR SHARING VIA ANY APP (SHARE API FROM REACT NATIVE)
+    const shareMessage = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     return (
         <View style={styles.container}>
             <Text>Person ith</Text>
             <Text>His Message</Text>
+            <Card
+                featuredTitle={PEOPLE[personId].name}
+                //! Image see
+                image={require('./images/alberto.png')}
+            >
+                <Text style={{ margin: 10 }}>{PEOPLE[personId].description}</Text>
+                <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, alignItems: "center" }}>
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareMessage(PEOPLE[personId].name, PEOPLE[personId].description, 'www.wikipedia.com')} />
+                    </View>
+                    <View style={{ flex: 1, alignItems: "flex-start" }}>
+                        <Text>Heartfelt?</Text>
+                        <Text>Share this message...</Text>
+                    </View>
+                </View>
+            </Card>
+            <Button
+                title="GO TO Next Person i+1"
+                onPress={() => nextPerson()}
+            />
+            <Button
+                title="Back To Home"
+                onPress={() => navigation.navigate('Home')}
+            />
         </View>
     );
 }
@@ -16,5 +73,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#69420f',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    cardItem: {
     },
 });
